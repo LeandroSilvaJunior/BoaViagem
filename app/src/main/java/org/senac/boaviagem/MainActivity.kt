663f4a778d7276c749d53a4cc79b2c14a1178033
Leandro da Silva Junior <leandro.sj.dev@gmail.com>
2020-04-05 21:20:08 -0300

    tela de login

--- app/src/main/java/org/senac/boaviagem/MainActivity.kt
+++ app/src/main/java/org/senac/boaviagem/MainActivity.kt
@@ -2,11 +2,17 @@ package org.senac.boaviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var logoComponent : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logoComponent = findViewById<ImageView>(R.id.iv_logo_app)
+        logoComponent.setImageResource(R.drawable.logo)
     }
 }
